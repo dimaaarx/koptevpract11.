02c984baaaf4a75e322acee62a3a7346d977efac
dimaaarx <koptevdima1107@gmail.com>
2026-06-10 01:52:42 +0300

    Update UserIdentifier.java

--- src/UserIdentifier.java
+++ src/UserIdentifier.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class UserIdentifier {
+    private int id;
+    private String name;
+
+    public UserIdentifier(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        UserIdentifier that = (UserIdentifier) obj;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
